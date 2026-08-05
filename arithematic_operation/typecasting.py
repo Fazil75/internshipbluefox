df1fc1637fce01aa9613be03aef71ecb1a5d4180
Your Name <your.email@example.com>
2026-08-05 20:00:47 +0530

    typecasting

---
+++ arithematic_operation/typecasting.py
@@ -0,0 +1,12 @@
+def add_num():
+    var1=input("Enter first number: ")
+    var2=input("Enter second number: ")
+
+  
+
+    var1=int(var1)
+    var2=int(var2)
+
+    print("sum  is", var1+var2)   
+
+add_num()
