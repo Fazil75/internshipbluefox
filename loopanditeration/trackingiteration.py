8a3bfe699c67345eb3b9278fa6f01429b61463da
Your Name <your.email@example.com>
2026-08-09 19:58:54 +0530

    tracking iteration

---
+++ loopanditeration/trackingiteration.py
@@ -0,0 +1,7 @@
+def main():
+    previous = 0
+    for current in range(10):
+     total = current + previous
+     print(f"Current {current} Previous {previous} Sum {total}")
+     previous = current
+main()
